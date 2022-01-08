2463d1c1504633c14d1b268c1ddf80c5c22ea143
someaddons <someaddons@github.com>
2022-01-08 14:11:59 +0100

    sodium compat

---
+++ src/main/java/com/betterfpsdist/mixin/MixinConfig.java
@@ -0,0 +1,61 @@
+package com.betterfpsdist.mixin;
+
+import net.fabricmc.loader.api.FabricLoader;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class MixinConfig implements IMixinConfigPlugin
+{
+    @Override
+    public void onLoad(final String mixinPackage)
+    {
+
+    }
+
+    @Override
+    public String getRefMapperConfig()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(final String targetClassName, final String mixinClassName)
+    {
+        if (targetClassName.equals("net.minecraft.client.renderer.LevelRenderer"))
+        {
+            return !FabricLoader.getInstance().isModLoaded("magnesium") &&
+                     !FabricLoader.getInstance().isModLoaded("sodium");
+        }
+
+        return FabricLoader.getInstance().isModLoaded("magnesium") ||
+                 FabricLoader.getInstance().isModLoaded("sodium");
+    }
+
+    @Override
+    public void acceptTargets(final Set<String> myTargets, final Set<String> otherTargets)
+    {
+
+    }
+
+    @Override
+    public List<String> getMixins()
+    {
+        return null;
+    }
+
+    @Override
+    public void preApply(final String targetClassName, final ClassNode targetClass, final String mixinClassName, final IMixinInfo mixinInfo)
+    {
+
+    }
+
+    @Override
+    public void postApply(final String targetClassName, final ClassNode targetClass, final String mixinClassName, final IMixinInfo mixinInfo)
+    {
+
+    }
+}
