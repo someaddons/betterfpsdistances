c75268b8f754001d94960ed3e247ad3108749c09
someaddons <someaddons@github.com>
2023-01-18 12:49:09 +0100

    1.19.3

--- src/main/java/com/betterfpsdist/mixin/SodiumRenderManagerMixin.java
+++ src/main/java/com/betterfpsdist/mixin/SodiumRenderManagerMixin.java
@@ -12,6 +12,9 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+/**
+ * Unused for now
+ */
 @Mixin(RenderSectionManager.class)
 public class SodiumRenderManagerMixin
 {
